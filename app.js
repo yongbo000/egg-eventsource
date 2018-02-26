5f5a094641989135911abed156ccee3cc6701339
jambo <yongbo.hello@gmail.com>
2018-02-26 22:58:01 +0800

    feat log when exception

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const EventStream = require('./lib/eventstream');
 module.exports = app => {
   app.config.coreMiddleware.push('eventsource');
   app.eventsource = new EventStream({
+    logger: app.logger,
     heartbeat: app.config.eventsource.heartbeat,
   });
 };
